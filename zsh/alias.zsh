e1c7ffe55860c71d328bb3e9cfaebd71fc6b4631
Kenji Fujiwara <kenji-f@is.naist.jp>
2012-07-16 15:55:10 +0900

    MacVimの設定を変更

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -4,6 +4,11 @@ case "${OSTYPE}" in
 darwin*)
 	alias ls="ls -hF -G"
 	alias xargs="gxargs"
+	#for use macvim on CUI
+	MacVim=~/Applications/MacVim.app/Contents/MacOS
+	alias vim='env $MacVim/Vim -u $HOME/.vimrc "$@"'
+	alias gvim='$MacVim/mvim --remote-tab-silent "$@"'
+	alias gitx=/Applications/GitX.app/Contents/MacOS/gitx
 	;;
 linux*)
 	alias ls="ls -hF --color=auto"
