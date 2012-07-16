480043df4bab8e99fcb52754db1077109bf36ff3
Kenji Fujiwara <kenji-f@is.naist.jp>
2012-07-16 15:58:38 +0900

    homebrew用のPATHを設定

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,5 +1,13 @@
 #for mac ports
-export PATH=/opt/local/bin:/opt/local/sbin:$PATH
+#export PATH=/opt/local/bin:/opt/local/sbin:$PATH
+
+#for homebrew
+export PATH=/usr/local/bin:$PATH
+export PATH=/usr/local/share/python:$PATH
+#export PATH=$PATH:PATH=/usr/local/bin
+
+#for tex
+export PATH=$PATH:/usr/local/texlive/2011/bin/x86_64-darwin
 
 #for my scripts
 export PATH=~/local/bin:$PATH
