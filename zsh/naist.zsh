59b32af082b93738a11e5cb4feebd0d3c4967e0a
Kenji Fujiwara <niyaton@gmail.com>
2016-11-18 20:46:34 +0900

    clean up naist.zsh

--- zsh/naist.zsh
+++ zsh/naist.zsh
@@ -1,17 +1,5 @@
-#create tunnel to sd-dev.naist.jp
-function tunnel_sd-dev(){
-  ssh -f -L 9999:sd-dev.naist.jp:22 sh.naist.jp sleep 10
-  echo "tunnel to sd-dev.naist.jp:22 is now available on port 9999 (only 10 sec.)"
-}
-
-#create tunnel to sd-dev.naist.jp for VM
-function tunnel_sd-dev_vm(){
-  ssh -f -L 9999:sd-dev.naist.jp:22 sh.naist.jp -g sleep 10
-  echo "tunnel to sd-dev.naist.jp:22 is now available on port 9999 (only 10 sec.)" 
-}
-
 #ssh connection via sh.naist.jp
-function tssh(){                                                                                                              
+function tssh(){
   echo create tunnel to $1
   ssh -f -L 10000:$1:22 sh.naist.jp -g sleep 10
   echo connect to $1
