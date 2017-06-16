45b891d70a3d5f3860262cbe6cf37d753c5d30fd
Kenji Fujiwara <niyaton@gmail.com>
2017-06-16 12:45:06 +0900

    move to os dependency section

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -4,6 +4,8 @@ case "${OSTYPE}" in
 darwin*)
 	alias ls="gls -hF --color=auto"
 	alias xargs="gxargs"
+	alias vim="reattach-to-user-namespace vim"
+	alias gvim="reattach-to-user-namespace gvim --remote-silent"
 	#for use macvim on CUI
 	#alias vim='env $MacVim/Vim -u $HOME/.vimrc "$@"'
 	#alias gvim='$MacVim/mvim --remote-tab-silent "$@"'
@@ -18,5 +20,3 @@ alias la="ls -a"
 alias usejp="export LANG=ja_JP.UTF-8"
 alias unusejp="export LANG="
 alias tmux="tmux -2u"
-alias vim="reattach-to-user-namespace vim"
-alias gvim="reattach-to-user-namespace gvim --remote-silent"
