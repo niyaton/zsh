c70ed4c25fb91583663f8a03d6a8a4da95f80e04
Kenji Fujiwara <niyaton@gmail.com>
2017-06-16 12:45:06 +0900

    move to os dependency section

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -4,6 +4,8 @@ case "${OSTYPE}" in
 darwin*)
 	alias ls="gls -hF --color=auto"
 	alias xargs="gxargs"
+	alias vim="reattach-to-user-namespace vim"
+	alias gvim="reattach-to-user-namespace gvim --remote-silent"
 	#for use macvim on CUI
 	#alias vim='env $MacVim/Vim -u $HOME/.vimrc "$@"'
 	#alias gvim='$MacVim/mvim --remote-tab-silent "$@"'
