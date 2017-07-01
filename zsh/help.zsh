5002afab31760f6b769c3f1131e71380be33498e
Kenji Fujiwara <niyaton@gmail.com>
2017-07-01 23:01:20 +0900

    add keybindings for run-help

--- zsh/help.zsh
+++ zsh/help.zsh
@@ -1,3 +1,5 @@
-unalias run-help
-autoload run-help
+alias run-hlep > /dev/null 2>&1 && unalias run-help
+autoload -U run-help
+bindkey -M vicmd '^[h' run-help
+bindkey -M viins '^[h' run-help
 HELPDIR=/usr/local/share/zsh/help
