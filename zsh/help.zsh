298d61114bfe104b2e322f08f0f42cece947d27a
Kenji Fujiwara <niyaton@gmail.com>
2017-07-07 01:47:41 +0900

    checkout from macos

---
+++ zsh/help.zsh
@@ -0,0 +1,13 @@
+alias run-hlep > /dev/null 2>&1 && unalias run-help
+autoload -U run-help
+bindkey -M vicmd '^[h' run-help
+bindkey -M viins '^[h' run-help
+
+case "${OSTYPE}" in
+	darwin*)
+		HELPDIR=/usr/local/share/zsh/help
+		;;
+	linux*)
+		HELPDIR=/usr/share/zsh/help
+		;;
+esac
