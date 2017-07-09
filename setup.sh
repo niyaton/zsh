a33f7b0f3c82a7b41c92031ccba2e5083adb8f64
Kenji Fujiwara <niyaton@gmail.com>
2017-07-10 02:41:32 +0900

    change dircolor on bow

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
 ln -i -n -s $PWD/zsh ~/.zsh
 ln -i -n -s $PWD/zshrc ~/.zshrc
-ln -i -n -s $PWD/Dracula.dircolors ~/.dircolors
+if [ -n "`uname -ar | grep Microsoft`" ]; then
+	ln -i -n -s $PWD/Dracula.dircolors.bow ~/.dircolors
+else
+	ln -i -n -s $PWD/Dracula.dircolors ~/.dircolors
+fi
