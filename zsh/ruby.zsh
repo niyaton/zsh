efb7219a062f79bdd7899152bd9221223a5a2f4a
Kenji Fujiwara <niyaton@gmail.com>
2017-07-09 20:01:28 +0900

    whence -w return none if command is not found

--- zsh/ruby.zsh
+++ zsh/ruby.zsh
@@ -1,6 +1,6 @@
 # Initialize rbenv.
 # Only execute eval rbenv init on startup
-if [[ "$(whence -w rbenv 2>/dev/null )" != *function ]]; then
+if [[ "$(whence -w rbenv 2>/dev/null )" != *(function|none) ]]; then
 	if which pyenv > /dev/null; then
 		eval "$(rbenv init -)"
 	fi
