f8fb65518234a9b75de1ae9abd8c403038dfe921
Kenji Fujiwara <niyaton@gmail.com>
2017-07-09 19:29:45 +0900

    refactored

--- zsh/00_path.zsh
+++ zsh/00_path.zsh
@@ -17,12 +17,4 @@ case "${OSTYPE}" in
 		;;
 esac
 
-# Initialize rbenv.
-# Only execute eval rbenv init on startup
-if [[ "$(whence -w rbenv 2>/dev/null )" != *function ]]; then
-	if which pyenv > /dev/null; then
-		eval "$(rbenv init -)"
-	fi
-fi
-
 typeset -U path
