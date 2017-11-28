af5f7ff55c923d2a62978fcac3ed9d69baa4738b
Kenji Fujiwara <niyaton@gmail.com>
2017-11-28 15:21:36 +0900

    add function for changing directory with fzf

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -5,3 +5,11 @@ function encrypt(){
 function decrypt(){
 	openssl aes-256-cbc -d -in $1 -out $1.plain
 }
+
+# fd - cd to selected directory
+fd() {
+  local dir
+  dir=$(find ${1:-.} -path '*/\.*' -prune \
+                  -o -type d -print 2> /dev/null | fzf +m) &&
+  cd "$dir"
+}
