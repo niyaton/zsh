32200bec494e677b5cc86e3a3827e5fe5d7622a5
Kenji Fujiwara <niyaton@gmail.com>
2017-11-28 15:19:30 +0900

    add dkrmi function

--- zsh/docker.zsh
+++ zsh/docker.zsh
@@ -30,3 +30,22 @@ function dkrm() {
     fi
     dkgrep $QUERY | xargs docker rm $*
 }
+
+function dkrmi() {
+    # TODO need to support <none>:<none>
+    local QUERY
+    if [[ $1 =~ "^-" ]]; then
+        QUERY=""            # options only
+    else
+        QUERY=$1            # with a query
+        [[ $# -gt 0 ]] && shift
+    fi
+
+    local FILTER
+    if [[ $QUERY == "" ]]; then
+        FILTER="fzf -m"
+    else
+        FILTER="fzf --query $QUERY"
+    fi
+    docker images | ${=FILTER} | awk '{ printf("%s:%s\n", $1, $2) }' | xargs docker rmi
+}
