ca22907441f0d64a4e215cdb271ece98c3251ef0
Kenji Fujiwara <niyaton@gmail.com>
2018-12-07 14:33:25 +0900

    immigrate to anaconda from pyenv

--- zsh/python.zsh
+++ zsh/python.zsh
@@ -1,6 +1,13 @@
-if which pyenv &> /dev/null; then eval "$(pyenv init -)"; fi
-# Initialize pyenv.
-# Only execute eval pyenv init on startup
-if [[ "$(whence -w pyenv 2>/dev/null )" != *(function|none) ]]; then
-	eval "$(pyenv init -)"
-fi
+case "${OSTYPE}" in
+  darwin*)
+    source ~/anaconda3/etc/profile.d/conda.sh
+    ;;
+  linux*)
+    if which pyenv &> /dev/null; then eval "$(pyenv init -)"; fi
+    # Initialize pyenv.
+    # Only execute eval pyenv init on startup
+    if [[ "$(whence -w pyenv 2>/dev/null )" != *(function|none) ]]; then
+           eval "$(pyenv init -)"
+    fi
+    ;;
+esac
