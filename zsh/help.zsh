3fe014e88964fb96744436e88f6993ba4a580ad1
Kenji Fujiwara <kenjif@tcu.ac.jp>
2021-09-23 02:27:09 +0900

    fix critical typo.

--- zsh/help.zsh
+++ zsh/help.zsh
@@ -1,4 +1,4 @@
-alias run-hlep > /dev/null 2>&1 && unalias run-help
+alias run-help > /dev/null 2>&1 && unalias run-help
 autoload -U run-help
 bindkey -M vicmd '^[h' run-help
 bindkey -M viins '^[h' run-help
